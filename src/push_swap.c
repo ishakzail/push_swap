3e6b54abf413b9e21904287828497ef3b7bd240a
ishakzail <ishak.zail.iz@gmail.com>
2022-02-09 15:22:55 +0100

    update for testing

--- src/push_swap.c
+++ src/push_swap.c
@@ -25,15 +25,29 @@ int main(int ac, char **av)
         ft_exit_ps("Wrong number of arguments", 1);
     strs = 0;
     i = split_args(&strs,av + 1," ");
-    printf("number of args == %d\n",i);
     a = fill_list(i,strs);
     check_dup(strs);
-    printStack(&a);
-    if (is_sorted(a) == 0)
-        printf("stack not sorted !\n");
-    else 
-       printf("stack a is sorted !\n");
-
+    // if (is_sorted(&a))
+    //     ft_exit_ps("Sorted successfully !",2);
+    // else
+    // sort_3(&a);
+    // else    
+    //     ft_exit_ps("Currenlty we are working only on 3 args",2);
     
+    // sort_stack(&a);
+    //ft_sx(a);
+    // printStack(&a);
+    
+    //ft_px(b,a);
+    // if (is_sorted(a) == 0)
+    //     printf("stack not sorted !\n");
+    // else 
+    //      printf("stack a is sorted !\n");
+    // is_sorted(&a) ? printf("==== YES ====\n") : printf(" ==== NO ====\n");
+    printf("arg1 == %d\n",ft_atoi(strs[0]));
+    ft_rrx(&a,&b);
+    printStack(&a);
+    // printStack(&b);
+
     return (0);
 }
