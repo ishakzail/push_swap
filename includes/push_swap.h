c2732a840c3e076b12e6952ee2648d60bef557ad
ishakzail <ishak.zail.iz@gmail.com>
2022-02-09 15:22:21 +0100

    some updates

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -27,27 +27,27 @@ t_stack    *ft_stacknew(int data);
 
 t_stack *pop(t_stack **s);
 t_stack *push(t_stack **s, int data);
+t_stack *stacklast(t_stack **s);
 
-void    ft_swap(t_stack *a);
-void    ft_sa(t_stack *s);
-void    ft_sb(t_stack *b);
-void    ft_ss(t_stack *a, t_stack *b);
-void    ft_pa(t_stack **a, t_stack **b);
-void    ft_pb(t_stack **a, t_stack **b);
-void    ft_rotate(t_stack **s);
-void    ft_ra(t_stack **a);
-void    ft_rb(t_stack **b);
-void    ft_rr(t_stack **a,t_stack **b);
-t_stack   *stacklast(t_stack **s);
 void    reverse(t_stack **s);
 void    pop_back(t_stack **s);
 void    pop_front(t_stack **s);
 void    push_back(t_stack **s, int data);
-void    push_front(t_stack **s, int data);
-void    ft_rra(t_stack **a);
-void    ft_rrb(t_stack **b);
-void    ft_rrr(t_stack **a, t_stack **b);
+ void    push_front(t_stack **s, int data);
 
+void    ft_swap(t_stack *s);
+// void    ft_sx(t_stack *s);
+void    ft_sx(t_stack **stack_1, t_stack **stack_2);
+// void    ft_ss(t_stack *a, t_stack *b);
+void    ft_px(t_stack **stack_1, t_stack **stack_2);
+// void    ft_rotate(t_stack **s);
+// void    ft_rx(t_stack **stack_1);
+void    ft_rx(t_stack **stack_1, t_stack **stack_2);
+// void    ft_rr(t_stack **stack_1, t_stack **stack_2);
+// void    reverse(t_stack **s);
+// void    ft_rrx(t_stack **stack_1);
+void    ft_rrx(t_stack **stack_1, t_stack **stack_2);
+// void    ft_rrr(t_stack **stack_1, t_stack **stack_2);
 
 /*************** Checks ***************/
 
@@ -66,4 +66,7 @@ void printStack(t_stack **s);
 
 void sort_stack(t_stack **s);
 
-int    is_sorted(t_stack *s);
+int    is_sorted(t_stack **s);
+
+void	sort_3(t_stack **stack);
+void	run(char *cmd, t_stack **stack_1, t_stack **stack_2, int x);
